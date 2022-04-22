cee1ddf9d25d4b15d0d63253b5a75c26ad96977f
Maxim Akimenko <maxnet663@gmail.com>
2022-04-22 18:51:25 +0300

    Task1 and Task2 Done

--- Task2/structs.h
+++ Task2/structs.h
@@ -1,59 +1,111 @@
-enum RoomsType {
-    kitchen,
-    bedroom, 
-    bathroom, 
-    childroom,
-    livingroom
+enum RoomType {
+    NON_ROOM = -1,
+    KITCHEN,
+    BEDROOM, 
+    BATHROOM, 
+    CHILDROOM,
+    LIVINGROOM
 };
 
-struct Rooms {
-    RoomsType type;
+enum BuildType {
+    ABL = -1,
+    HOUSE,
+    GARAGE,
+    BARN,
+    BATH
+};
+
+struct Room {
+
+    RoomType type;
     double area = 5;
 };
 
-struct Floors {
-    int amountRooms = 2;
+struct Floor {
+
+    void setAmountRooms(int a) {
+        if (a >= 2 && a <= 4)
+            amountRooms = a;
+    }
+
+    int getAmountRooms() {
+        return amountRooms;
+    }
+
     double height = 3;
-    std::vector<Rooms> rm;
+    std::vector<Room> rooms;
+    private:
+    int amountRooms = 2;
 };
 
-struct Bath {
-    bool exist = false;
-    double area = 0;
-    bool fireplace = true;
-};
+struct Building {
 
-struct Barn {
-    bool exist = false;
-    double area = 0;
-};
+    void isThereFireplace(bool a) {
+        if (type == HOUSE || type == BATH)
+            fireplace = a;
+    }
 
-struct Garage {
-    bool exist = false;
-    double area = 0;
-};
-struct House {
-    double area = 1;
-    bool fireplace = true;
-    int amountFloors = 1;
-    std::vector<Floors> fl;
-};
+    bool isThereFireplace() {
+        return fireplace;
+    }
 
-struct Buildings {
-    Garage gr;
-    Barn br;
-    Bath bt;
+    void setAmountFloors(int a) {
+        if (a >= 1 && a <= 3) 
+            amountfloors = a;
+    }
+
+    int getAmountFloors() {
+        return amountfloors;
+    }
+
+    bool isCorrectArea() {
+        double roomsArea = 0;
+        for (int a = 0; a < floors.size(); a++) {
+            for (int b = 0; b < floors[a].rooms.size(); b++) {
+                roomsArea += floors[a].rooms[b].area;
+            }
+        }
+        if (roomsArea / floors.size() > area) {
+            std::cout << "Summary area of the rooms is greater than house's area!" << std::endl;
+            return false;
+        }
+        return true;
+    }
+
+    BuildType type;
+    double area;
+    std::vector<Floor> floors;
+    private:
+    int amountfloors = 1;
+    bool fireplace;
 };
 
 struct Land {
+    double get_buildings_area() {
+        double buildingsArea = 0;
+        for (int k = 0; k < buildings.size(); k++) {
+            buildingsArea += buildings[k].area;
+        }
+        if (buildingsArea > area) {
+            std::cout << "Summary area of the buildings is greater than the site's area!" << std::endl;
+            return -1;
+        }
+        return buildingsArea;
+    }
+
     double area = 0;
     int amountOfBuildings = 1;
-    std::vector<Buildings> builds;
-    House home;
+    std::vector<Building> buildings;
 };
 
 struct Village {
-    double area = 0;
+    double get_village_area() {
+        double totalArea = 0;
+       for (int i = 0; i < sites.size(); i++) {
+           totalArea += sites[i].area;
+       }
+       return totalArea;
+    }
     int amountSites = 0;
     std::vector<Land> sites;
 };
--- Task2/task2.cpp
+++ Task2/task2.cpp
@@ -3,11 +3,173 @@
 #include <vector>
 #include "structs.h"
 
+std::string get_build_type(BuildType type) {
+    switch (type) {
+case HOUSE:
+        return "House";
+        break;
+case GARAGE:
+        return "Garage";
+        break;
+case BARN:
+        return "Barn";
+        break;
+case BATH:
+        return "Bath";
+        break;
+default:
+        return "Err";
+        break;
+    }
+}
+
+BuildType get_build_type(std::string s) {
+    if (s == "House" || s == "house")
+        return HOUSE;
+    if (s == "Garage" || s == "garage")
+        return GARAGE;
+    if (s == "Barn" || s == "barn")
+        return BARN;
+    if (s == "Bath" || s == "bath")
+        return BATH;
+    return ABL;
+}
+
+std::string get_room_type(RoomType type) {
+    switch (type) {
+        case KITCHEN:
+            return "kitchen";
+            break;
+        case BEDROOM:
+            return "bedroom";
+            break;
+        case CHILDROOM:
+            return "childroom";
+            break;
+        case LIVINGROOM:
+            return "livingroom";
+            break;
+        default:
+            return "Err";
+            break;
+    }
+}
+
+RoomType get_room_type(std::string s) {
+    if (s == "Kitchen" || s == "kitchen")
+        return KITCHEN;
+    if (s == "Bedroom" || s == "vedroom")
+        return BEDROOM;
+    if (s == "Childroom" || s == "childroom")
+        return CHILDROOM;
+    if (s == "Livingroom" || s == "livingroom")
+        return LIVINGROOM;
+    return NON_ROOM;
+}
+
+void set_properties(Building &build) {
+    switch (build.type) {
+        case HOUSE:
+            {
+                std::cout << "Enter the area of the house:" << std::endl;
+                std::cin >> build.area;
+                std::cout << "Is there a fireplace in the house?\n[y/n]:";
+                char ans;
+                std::cin >> ans;
+                build.isThereFireplace(ans == 'y');
+                std::cout << "How many floors are there in the house?" << std::endl;
+                int af;
+                std::cin >> af;
+                build.setAmountFloors(af);
+                for (int f = 0; f < build.getAmountFloors(); f++) {
+                    Floor currentFloor;
+                    std::cout << "The " <<  f + 1 << " floor:" << std::endl;
+                    std::cout << "Which is the floor height?" << std::endl;
+                    std::cin >> currentFloor.height;
+                    std::cout << "How many rooms are there on the floor?" << std::endl;
+                    int ar;
+                    std::cin >> ar;
+                    currentFloor.setAmountRooms(ar);
+                    for (int r = 0; r < currentFloor.getAmountRooms(); r++) {
+                        Room currentRoom;
+                        std::cout << "What is the type of the " << r + 1;
+                        std::cout << " room? (kitchen, bedroom, bathroom, childroom, livingroom):" << std::endl;
+                        std::string roomType;
+                        std::cin >> roomType;
+                        currentRoom.type = get_room_type(roomType);
+                        std::cout << "Whis is the area of the room?" << std::endl;
+                        std::cin >> currentRoom.area;
+                        currentFloor.rooms.push_back(currentRoom);
+                    }
+                    build.floors.push_back(currentFloor);
+                }
+            }
+            break;
+
+        case GARAGE:
+            {
+                std::cout << "Which is the area of the Garage:" << std::endl;
+                std::cin >> build.area;
+            } 
+            break;
+
+        case BARN:
+            {
+                std::cout << "Which is the area of the BARN:" << std::endl;
+                std::cin >> build.area;
+            }
+            break;
+
+            case BATH:
+            {
+                std::cout << "Which is the area of the Bath:" << std::endl;
+                std::cin >> build.area;
+                std::cout << "Is there a fireplace in the Bath:\n[y/n]:" << std::endl;
+                char ans;
+                std::cin >> ans;
+                build.isThereFireplace(ans == 'y');
+            }
+    }
+
+}
+
+double get_buildings_area(Village *vl) {
+    int buildingsArea = 0;
+    for (int i = 0; i < vl->sites.size(); i ++) {
+        buildingsArea += vl->sites[i].get_buildings_area();
+    }
+    return buildingsArea;
+}
+
+
 int main() {
     Village vl;
-    Land ln;
-    ln.area = 10;
-    vl.sites.push_back(ln);
-    std::cout << vl.sites[0].area << std::endl;
+    std::cout << "Enter how many sites in the village?" << std::endl;
+    std::cin >> vl.amountSites;
+    for (int i = 0; i < vl.amountSites; i++) {
+        Land currentSite;
+        std::cout << "The "  << i + 1 << " site:" << std::endl;
+        std::cout << "Which is the area of this site?" << std::endl;
+        std::cin >> currentSite.area;
+        std::cout << "How many buildings in this site?" << std::endl;
+        std::cin >> currentSite.amountOfBuildings;
+
+        for (int j = 0; j < currentSite.amountOfBuildings; j++) {
+            Building currentBuilding;
+            std::cout << "The "  << j + 1 << " building:" << std::endl;
+            std::cout << "Enter a type of the building (House, Garage, Barn, Bath):" << std::endl;
+            std::string buildType;
+            std::cin >> buildType;
+            currentBuilding.type = get_build_type(buildType);
+            set_properties(currentBuilding);
+            currentSite.buildings.push_back(currentBuilding);
+        }
+
+        vl.sites.push_back(currentSite);
+    }
+
+    std::cout << get_buildings_area(&vl) << std::endl;
+    std::cout << vl.get_village_area() << std::endl;
+
     return 0;
 }
